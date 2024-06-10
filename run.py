92acfce0e17c4b93cd8f8ca88ccc46b83492a94c
Stepan <31982882+lazoriks@users.noreply.github.com>
2024-06-10 10:55:34 +0100

    first step to create game

--- run.py
+++ run.py
@@ -1,1 +1,11 @@
+def play_game():
+    '''
+    The function runs the game, asks the player a question,
+    validates the response of the player and returns
+    the score of the player.
+    '''
+    places_list = read_file('places.txt')
+    countries_list = read_file('countries.txt')
+
+
 play_game()
