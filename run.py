d708efcec36d1246b3290bbeb0de5cc1df73d912
Stepan <31982882+lazoriks@users.noreply.github.com>
2024-06-10 11:03:29 +0100

    read file; ask name player

--- run.py
+++ run.py
@@ -1,3 +1,24 @@
+from random import randint
+from string import capwords
+
+def read_file(file):
+    '''
+    The function reads the file containing
+    the list of places or countries.
+    '''
+    with open(file, 'r') as f:
+        lines = f.readlines()
+    return [line.strip() for line in lines]
+
+def ask_name():
+    '''
+    The function gets the name of the player and
+    welcomes the player to the game.
+    '''
+    player_name = input('Please enter your name.\n')
+    print(f'***Welcome to the Famous Places and Countries game, {player_name}!***')
+    return player_name
+
 def play_game():
     '''
     The function runs the game, asks the player a question,
@@ -7,5 +28,9 @@ def play_game():
     places_list = read_file('places.txt')
     countries_list = read_file('countries.txt')
 
+    player_score = 0
+    question_answered_indexes = []
+
+    ask_name()
 
 play_game()
