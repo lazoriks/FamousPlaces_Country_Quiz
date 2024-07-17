5850d56c42471460a7338ab4babf83023e6813a2
Stepan <31982882+lazoriks@users.noreply.github.com>
2024-07-18 00:47:35 +0100

    fix index and package

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 const total = require('total4');
 const http = require('http');
 
-// Створення серверу
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 8000;  
+
 
 const app = total.HTTP('debug');
+
 
 app.route('/', function() {
     this.plain('Hello, World!');
 });
+
 
 http.createServer(app).listen(port, () => {
     console.log(`Server is running on port ${port}`);
