0b335955fabca6d9f96e46486200ff254bef108e
Stepan <31982882+lazoriks@users.noreply.github.com>
2024-07-18 00:57:37 +0100

    fix index and package 3

--- index.js
+++ index.js
@@ -1,17 +1,30 @@
-const total = require('total4');
-const http = require('http');
+// ===================================================
+// Total.js start script
+// https://www.totaljs.com
+// ===================================================
 
-const port = process.env.PORT || 8000;  
+const options = {};
 
+// options.ip = '127.0.0.1';
+options.port = parseInt(process.env.PORT);
+// options.unixsocket = require('path').join(require('os').tmpdir(), 'app_name');
+// options.config = { name: 'Total.js' };
+// options.sleep = 3000;
+// options.inspector = 9229;
+// options.watch = ['private'];
+// options.livereload = 'https://yourhostname';
 
-const app = total.HTTP('debug');
+// Enables cluster:
+// options.cluster = 'auto';
+// options.cluster_limit = 10; // max 10. threads (works only with "auto" scaling)
 
+// Enables threads:
+// options.cluster = 'auto';
+// options.cluster_limit = 10; // max 10. threads (works only with "auto" scaling)
+// options.timeout = 5000;
+// options.threads = '/api/';
+// options.logs = 'isolated';
 
-app.route('/', function() {
-    this.plain('Hello, World!');
-});
-
-
-http.createServer(app).listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-});
+var type = process.argv.indexOf('--release', 1) !== -1 || process.argv.indexOf('release', 1) !== -1 ? 'release' : 'debug';
+// require('total4/' + type)(options);
+require('total4').http('release', options);
