d5f18fc2bf3cda3451c8a7574f428c658d76ab9d
Stepan <31982882+lazoriks@users.noreply.github.com>
2024-07-23 10:21:44 +0100

    fix bug for deploy in heroku

--- index.js
+++ index.js
@@ -5,15 +5,26 @@
 
 const options = {};
 
+// options.ip = '127.0.0.1';
 options.port = parseInt(process.env.PORT);
+// options.unixsocket = require('path').join(require('os').tmpdir(), 'app_name');
+// options.config = { name: 'Total.js' };
+// options.sleep = 3000;
+// options.inspector = 9229;
+// options.watch = ['private'];
+// options.livereload = 'https://yourhostname';
 
-const type = process.argv.indexOf('--release', 1) !== -1 || process.argv.indexOf('release', 1) !== -1 ? 'release' : 'debug';
+// Enables cluster:
+// options.cluster = 'auto';
+// options.cluster_limit = 10; // max 10. threads (works only with "auto" scaling)
 
-// Start Total.js HTTP server
-const total = require('total4');
+// Enables threads:
+// options.cluster = 'auto';
+// options.cluster_limit = 10; // max 10. threads (works only with "auto" scaling)
+// options.timeout = 5000;
+// options.threads = '/api/';
+// options.logs = 'isolated';
 
-// Add a route for the root path
-total.http(type, options);
-F.route('/', function() {
-    this.plain('Hello, Total.js!');
-});
+var type = process.argv.indexOf('--release', 1) !== -1 || process.argv.indexOf('release', 1) !== -1 ? 'release' : 'debug';
+// require('total4/' + type)(options);
+require('total4').http('release', options);
